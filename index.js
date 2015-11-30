671eea266873b1d84c93f2c061ecc760877c48ca
Andrija Petrovic <andrija.hers@gmail.com>
2015-11-30 12:55:36 +0100

    More helpers

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ function createlib (execlib) {
     PromiseHistoryChainerJob = require('./promisehistorychainerjobcreator')(execlib, JobBase, PromiseChainerJob),
     PromiseMapperJob = require('./promisemapperjobcreator')(execlib, JobBase, PromiseChainerJob);
 
+  function returner(val) {
+    return function() {
+      return val;
+    }
+  };
   function applier(fn, ctx) {
     return function (arry) {
       return fn.apply(ctx, arry);
     };
+  }
+  function promise2defer(promise, defer) {
+    promise.then(
+      defer.resolve.bind(defer),
+      defer.reject.bind(defer),
+      defer.notify.bind(defer)
+    );
+    return defer.promise;
   }
   return {
     chainPromises : require('./chainpromises')(execlib),
@@ -20,7 +33,9 @@ function createlib (execlib) {
     PromiseHistoryChainerJob: PromiseHistoryChainerJob,
     PromiseMapperJob: PromiseMapperJob,
     JobCollection: require('./jobcollectioncreator')(execlib),
-    applier: applier
+    returner: returner,
+    applier: applier,
+    promise2defer: promise2defer
   };
 }
 
