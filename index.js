075c548290030c4fbb76343ec75ce848e6660c65
Andrija Petrovic <andrija.hers@gmail.com>
2016-05-26 11:48:23 +0000

    Closure leak fixes

--- index.js
+++ index.js
@@ -13,37 +13,58 @@ function createlib (execlib) {
 
   function returner(val) {
     return function() {
-      return q(val);
+      var ret = q(val);
+      val = null;
+      return ret;
     }
   };
   function propertyreturner(obj, propertyname) {
+    var _q = q;
     return function () {
-      return q(obj[propertyname]);
+      var ret = _q(obj[propertyname]);
+      obj = null;
+      propertyname = null;
+      _q = null;
+      return ret;
     }
   }
   function resultpropertyreturner(propertyname) {
+    var _q = q;
     return function (result) {
+      var ret;
       if (result && 'object' === typeof result && result.hasOwnProperty(propertyname)) {
-        return q(result[propertyname]);
+        ret = _q(result[propertyname]);
       } else {
-        return q(null);
+        ret = _q(null);
       }
+      propertyname = null;
+      _q = null;
+      return ret;
     }
   }
   function executor(fn, ctx) {
     return function () {
-      return fn.call(ctx);
+      var ret = fn.call(ctx);
+      fn = null;
+      ctx = null;
+      return ret;
     }
   }
   function applier(fn, ctx) {
     return function (arry) {
-      return fn.apply(ctx, arry);
+      var ret = fn.apply(ctx, arry);
+      fn = null;
+      ctx = null;
+      return ret;
     };
   }
   function methodinvoker(methodname) {
     var args = Array.prototype.slice.call(arguments, 1);
     return function (instance) {
-      return q(instance[methodname].apply(instance, args));
+      var ret = q(instance[methodname].apply(instance, args));
+      methodname = null;
+      args = null;
+      return ret;
     }
   }
   function promise2defer(promise, defer) {
@@ -58,15 +79,20 @@ function createlib (execlib) {
     console.error(reason);
   }
   function promise2execution(promise, cb, errcb, notifycb) {
+    var _q = q;
     return promise.then(
       function(){
+        var ret;
         try{
-          return cb();
+          ret = cb();
         } catch(e) {
           console.error(e.stack);
           console.error(e);
-          return q.reject(e);
+          ret = _q.reject(e);
         }
+        cb = null;
+        _q = null;
+        return ret;
       },
       errcb || standardErrReporter,
       notifycb || lib.dummyFunc
